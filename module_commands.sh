70e6206ce996f1c5833e389c9821b04739999c47
Vikas Kurapati <vikas.kurapati@tum.de>
2023-07-26 08:36:51 +0200

    Add files via upload

---
+++ module_commands.sh
@@ -0,0 +1,22 @@
+module load intel-oneapi-compilers/2021.4.0
+module load gcc/11.2.0
+module load cmake/3.21.4
+module load python/3.8.11-extended
+module load libszip/2.1.1
+module load parmetis/4.0.3-intel21-impi-i64-r64
+module load metis/5.1.0-intel21-i64-r64
+module load numactl/2.0.14-intel21
+module load yaml-cpp/0.7.0-intel21
+module load julia/1.7.2
+module load netcdf-hdf5-all/4.6_hdf5-1.10-intel21-impi
+
+
+#### Do these
+module load intel-oneapi-compilers/2021.4.0
+module load gcc/11.2.0
+module load cmake/3.21.4
+module load python/3.8.11-extended
+module load nano
+
+source ~/.bashrc
+https://github.com/vikaskurapati/SeisSol.git
